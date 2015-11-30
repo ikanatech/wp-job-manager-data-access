5e253e65aabeca6c23f3ad77999b691092e4c122
Spencer Heckathorn <spencer@ikanatech.com>
2015-11-30 09:54:13 -0600

    Create wpjm-da.php
    Added my code to github... This is the first time to add code here and I think I may have made it a little more messy.

---
+++ wpjm-da.php
@@ -0,0 +1,38 @@
+<?php
+
+/*******************
+/       Some stuff for trying to "free" jobs
+********************/
+
+add_filter( 'job_manager_output_jobs_defaults', array( $this, 'job_manager_output_jobs_defaults' ) );
+function job_manager_output_jobs_defaults( $default ) {
+        $type = get_queried_object();
+
+        if ( is_tax( 'job_listing_type' ) ) {
+                $default[ 'job_types' ] = $type->slug;
+                $default[ 'selected_job_types' ] = $type->slug;
+                $default[ 'show_categories' ] = true;
+
+        }
+        elseif ( is_tax( 'job_listing_category' ) ) {
+                $default[ 'show_categories' ] = true;
+                $default[ 'categories' ] = $type->slug;
+                $default[ 'selected_category' ] = $type->slug;
+        }
+        elseif ( is_search() ) {
+                $default[ 'keywords' ] = get_search_query();
+                $default[ 'show_filters' ] = false;
+        }
+
+        if ( is_home() || is_page_template( 'page-templates/test.php' ) ) {
+                $default[ 'show_category_multiselect' ] = false;
+        }
+
+        if ( isset( $_GET[ 'search_categories' ] ) ) {
+                $category = get_term_by( 'ID', absint( $_GET[ 'search_categories' ] ), 'job_listing_cat$
+                $default[ 'show_categories' ] = true;
+                $default[ 'categories' ] = $_GET[ 'search_categories' ];
+        }
+
+      return $default;
+}
